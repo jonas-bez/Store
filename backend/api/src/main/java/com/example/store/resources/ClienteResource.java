7113c7c48f58e7a4284d3afcc731276d35047474
jonas-bez <jonas.bezzerra@gmail.com>
2022-01-24 06:59:53 -0300

    ajustes nos endpoints

--- backend/api/src/main/java/com/example/store/resources/ClienteResource.java
+++ backend/api/src/main/java/com/example/store/resources/ClienteResource.java
@@ -2,6 +2,8 @@ package com.example.store.resources;
 
 import com.example.store.model.Cliente;
 import com.example.store.repository.ClienteRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,31 +11,38 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Clientes")
+@CrossOrigin(origins = "*")
 public class ClienteResource {
 
     @Autowired
     ClienteRepository clienteRepository;
 
+    @ApiOperation(value="Retorna uma lista de clientes")
     @GetMapping("/cliente")
-    public List<Cliente> listarProdutos(){
+    public List<Cliente> listarCliente(){
         return clienteRepository.findAll();
     }
 
+    @ApiOperation(value="Retorna um cliente unico")
     @GetMapping("/cliente/{id}")
-    public Cliente listarUmProduto(@PathVariable(value = "id") long id){
+    public Cliente listarUmCliente(@PathVariable(value = "id") long id){
         return clienteRepository.findById(id);
     }
 
+    @ApiOperation(value="Salva um cliente")
     @PostMapping("/cliente")
-    public Cliente salvaProduto(@RequestBody Cliente cliente ){
+    public Cliente salvaCliente(@RequestBody Cliente cliente ){
         return clienteRepository.save(cliente);
     }
 
+    @ApiOperation(value="Deleta um cliente")
     @DeleteMapping("/cliente")
-    public void deletaProduto(@RequestBody Cliente cliente) {  clienteRepository.delete(cliente); }
+    public void deletaCliente(@RequestBody Cliente cliente) {  clienteRepository.delete(cliente); }
 
+    @ApiOperation(value="Atualiza um cliente")
     @PutMapping("/cliente")
-    public void atualizarProduto(@RequestBody Cliente cliente) {
+    public void atualizarCliente(@RequestBody Cliente cliente) {
         clienteRepository.save(cliente);
     }
 }
